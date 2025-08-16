b92088578a46be7a7731c1d6d7aef135e04a82f7
Rob <randymalone95@yahoo.com>
2025-08-16 15:33:33 -0400

    fixed mobile and various things

--- web/credits.js
+++ web/credits.js
@@ -1,6 +1,7 @@
 var creditsDiv = document.createElement("div");
 creditsDiv.id = "credits-div";
-document.body.appendChild(creditsDiv);
+var body0 = document.getElementById("body_0");
+body0.appendChild(creditsDiv);
 
 fetch("credits.html")
   .then((response) => response.text())
